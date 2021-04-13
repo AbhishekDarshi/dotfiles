78d1a9feb8ea74131c1e5b52eeed379d73440abe
Abhishek D R <45519519+AbhishekDarshi@users.noreply.github.com>
2021-04-13 18:32:49 +0000

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -182,8 +182,39 @@
 		    (unless (eq ibuffer-sorting-mode 'recency)
 		      (ibuffer-do-sort-by-recency))))
 
+
+;; docker
+;; https://www.emacswiki.org/emacs/TrampAndDocker
 (use-package dockerfile-mode
   :defer t)
+
+(use-package docker
+  :bind ("C-c d" . docker))
+
+(use-package docker-tramp)
+
+;; Open files in Docker containers like so: /docker:drunk_bardeen:/etc/passwd
+(push
+ (cons
+  "docker"
+  '((tramp-login-program "docker")
+    (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+    (tramp-remote-shell "/bin/sh")
+    (tramp-remote-shell-args ("-i") ("-c"))))
+ tramp-methods)
+
+(defadvice tramp-completion-handle-file-name-all-completions
+  (around dotemacs-completion-docker activate)
+  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+    a list of active Docker container names, followed by colons."
+  (if (equal (ad-get-arg 1) "/docker:")
+      (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
+             (dockernames (cl-remove-if-not
+                           #'(lambda (dockerline) (string-match ":$" dockerline))
+                           (split-string dockernames-raw "\n"))))
+        (setq ad-return-value dockernames))
+    ad-do-it))
+
 (use-package yaml-mode
   :defer t)
 (use-package web-mode
@@ -536,11 +567,26 @@
   (add-hook 'prog-mode-hook 'yas-minor-mode)
   (add-hook 'text-mode-hook 'yas-minor-mode))
 
+(use-package vterm)
+(use-package multi-vterm)
+
+
+(use-package perspective
+  :bind (("C-c b" . persp-switch-to-buffer*)
+  :config
+  (persp-mode))
+
+(global-set-key (kbd "C-c C-b") (lambda (arg)
+                                  (interactive "P")
+                                  (if (fboundp 'persp-ibuffer)
+                                      (persp-ibuffer arg)
+                                    (ibuffer "all"))))
+
 ;; todo
 
 ;; hydra
 ;; session restore
-;; dap mode
-;; tramp
-;; workspaces
+;; dap mode - add keybindings
+;; tramp - added for docker settings.
+;; workspaces - perspective package should take care of basics.
 
